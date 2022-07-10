5d603333742821f60cd19370b294c9d8f571ea6e
Dax Harris <matteovh@gmail.com>
2022-07-10 19:27:39 -0400

    state setup

--- server/main.py
+++ server/main.py
@@ -1,10 +1,19 @@
 from starlite import Starlite
 from starlite.datastructures import State
+from pymongo.mongo_client import MongoClient
 from util import *
+
 
 def setup_state(state: State):
     state.config = Config()
+    mncli = MongoClient(
+        host=state.config["database"]["host"],
+        port=state.config["database"]["port"],
+        username=state.config["database"]["username"],
+        password=state.config["database"]["password"],
+        tls=state.config["database"]["tls"],
+    )
+    state.database = mncli[state.config["database"]["database"]]
 
-    print(state.config.data)
 
 app = Starlite(on_startup=[setup_state], route_handlers=[])
