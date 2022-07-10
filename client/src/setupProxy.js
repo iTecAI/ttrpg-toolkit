c4717c2a3890540169ad9d0ac9988078aff89ea8
Dax Harris <matteovh@gmail.com>
2022-07-10 17:40:50 -0400

    project setup

---
+++ client/src/setupProxy.js
@@ -0,0 +1,15 @@
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+    app.use(
+        createProxyMiddleware("/api", {
+            target: "http://127.0.0.1:8000",
+            pathRewrite: {
+                "^/api": "/",
+            },
+            headers: {
+                Connection: "keep-alive",
+            },
+        })
+    );
+};
