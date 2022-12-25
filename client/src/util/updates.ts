6fe4050f4d1aa053d700bd99b7a19f53916cfdcb
Dax Harris <dax.harris@bryx.com>
2022-12-25 08:33:26 -0500

    long polling tests

--- client/src/util/updates.ts
+++ client/src/util/updates.ts
@@ -1,20 +1,36 @@
+import { ApiResponse, get } from "./api";
+
 export type UpdateType = {
     event: string;
-    data: { [key: string]: any };
-    dispatched: number;
+    body: { [key: string]: any } | null;
 };
 
 export class Updates {
-    private source: EventSource;
     private events: UpdateType[];
     public waiting_events: number;
+    private retry: number;
     constructor(private session: string) {
-        this.source = new EventSource(`/api/updates/subscribe/${session}`);
         this.events = [];
         this.waiting_events = 0;
+        this.retry = 0;
+        this.run();
+    }
 
-        this.source.addEventListener("open", console.log);
-        this.source.addEventListener("message", console.log);
+    private async run() {
+        let result: ApiResponse<UpdateType>;
+        while (true) {
+            result = await get<UpdateType>("/updates/poll");
+            if (result.success) {
+                console.log(result.value);
+            } else {
+                console.warn(
+                    `Polling error ${result.statusCode}: ${result.debugMessage}. Waiting ${this.retry}s for retry.`
+                );
+                setTimeout(this.run, this.retry * 1000);
+                this.retry += 5;
+                break;
+            }
+        }
     }
 }
 
--- server/controllers/update_controller.py
+++ server/controllers/update_controller.py
@@ -1,11 +1,11 @@
 from starlite import Controller, get, post, State, Provide, Request
 from starlite.response import StreamingResponse
-from util import Cluster, Session
+from util import Cluster, Session, session_dep, guard_loggedIn
 from util.exceptions import AuthorizationFailedError
-from typing import TypedDict, Any
+from typing import TypedDict, Any, Optional
 import json
 import time
-from sse_starlette.sse import EventSourceResponse
+from pydantic import BaseModel
 from asyncio.queues import QueueEmpty
 
 
@@ -15,47 +15,23 @@ class EventType(TypedDict):
     dispatched: float
 
 
-def encode(data):
-    return {"data": json.dumps(data)}
-
-
-async def flush(request: Request):
-    CHUNK = "." * 8192**2
-    await request.send(
-        {
-            "type": "http.response.body",
-            "body": f": {CHUNK}\n\n".encode(),
-            "more_body": True,
-        }
-    )
+class PollEvent(BaseModel):
+    event: str
+    body: Optional[dict[str, Any]] = None
 
 
 class UpdateController(Controller):
     path: str = "/updates"
 
-    @get("/subscribe/{sessionId:str}")
-    async def subscribe(
-        self, state: State, sessionId: str, request: Request
-    ) -> EventSourceResponse:
-        session = Session.load_oid(sessionId, state.database)
-        if session == None:
-            raise AuthorizationFailedError()
+    @get(
+        "/poll", guards=[guard_loggedIn], dependencies={"session": Provide(session_dep)}
+    )
+    async def poll(self, session: Session, state: State) -> PollEvent:
         cluster: Cluster = state.cluster
         cluster.ensure_queue(session.oid)
 
-        async def publisher(req: Request):
-            while session.oid in cluster.event_queues.keys():
-                try:
-                    event: EventType = cluster.event_queues[session.oid].get_nowait()
-                    print(event)
-                    await flush(req)
-                    yield encode(event)
-                except QueueEmpty:
-                    if round(time.time()) % 30 == 0:
-                        await flush(req)
-                        yield encode(f"ping {time.ctime()}")
-
-                time.sleep(0.25)
-
-        gen = publisher(request)
-        return EventSourceResponse(gen)
+        event: EventType = await cluster.event_queues[session.oid].get()
+        return PollEvent(
+            event=event["event"],
+            body=event["data"] if len(event["data"].keys()) > 0 else None,
+        )
