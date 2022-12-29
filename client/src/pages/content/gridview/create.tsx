98d9d5c2356eb65a7c95ff4b3bb0a70e33bddd9a
Dax Harris <dax.harris@bryx.com>
2022-12-29 17:16:01 -0500

    styling changes

--- client/src/pages/content/gridview/create.tsx
+++ client/src/pages/content/gridview/create.tsx
@@ -1,7 +1,13 @@
 import { IconButton, Paper, Tooltip, Typography } from "@mui/material";
 import "./index.scss";
 import { Box } from "@mui/system";
-import { MdAdd, MdCreateNewFolder, MdFolder } from "react-icons/md";
+import {
+    MdAdd,
+    MdCreateNewFolder,
+    MdDescription,
+    MdMap,
+    MdPersonAdd,
+} from "react-icons/md";
 import { loc } from "../../../util/localization";
 import { ReactNode } from "react";
 import { Masonry } from "@mui/lab";
@@ -13,7 +19,7 @@ function CreateButton(props: {
     return (
         <Box className="btn-container">
             <Tooltip title={props.tooltip} disableInteractive>
-                <IconButton className="create-btn" color="primary" size="large">
+                <IconButton className="create-btn" color="default" size="large">
                     {props.icon}
                 </IconButton>
             </Tooltip>
@@ -30,11 +36,26 @@ export function CreateGridViewItem(props: {}): JSX.Element {
                     <Typography variant="overline" className="text">
                         {loc("content.create.new")}
                     </Typography>
-                    <Paper elevation={2} className="add-buttons">
+                    <Paper elevation={1} className="add-buttons">
+                        <Typography variant="overline" className="types-title">
+                            {loc("content.create.types")}
+                        </Typography>
                         <Masonry spacing={2} columns={3}>
                             <CreateButton
                                 icon={<MdCreateNewFolder size={32} />}
                                 tooltip={loc("content.create.folder")}
+                            />
+                            <CreateButton
+                                icon={<MdDescription size={32} />}
+                                tooltip={loc("content.create.document")}
+                            />
+                            <CreateButton
+                                icon={<MdPersonAdd size={32} />}
+                                tooltip={loc("content.create.character")}
+                            />
+                            <CreateButton
+                                icon={<MdMap size={32} />}
+                                tooltip={loc("content.create.map")}
                             />
                         </Masonry>
                     </Paper>
